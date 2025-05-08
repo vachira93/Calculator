f710fe3fdd7c5db8d942152f1bcbeee24d1f2c6a
Vachira Sarapattana <Vachira93@gmail.com>
2025-05-08 21:15:43 +0700

    add calculator gui

---
+++ calculator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk,messagebox
+
+GUI = TK()
+GUI.title('Basic Git & Github by Tai58')
+GUI.geometry('500x700')
+
+L = Label(GUI,text= 'Tai Amnuay Silpa')
+L.pack()
+
+GUI.mainloop()
